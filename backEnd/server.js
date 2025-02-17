36c41d68db29c5fa4cb50ab1cfa24946fd6698b1
isaacY0573 <isaacjosef53@gmail.com>
2025-02-17 14:27:32 +0200

    Initial commit

---
+++ backEnd/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+
+
+
+console.log('Server is running on port 3000');
+
+
+
+app.listen(3000);
---
+++ frontEnd/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
+const App = () => {
+  return (
+    <div>
+   
+    </div>
+  )
+}
+
+export default App
