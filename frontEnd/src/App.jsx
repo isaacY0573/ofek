5528af937bde80124d5d56521705ba9e0106fa53
yosef_efrati <yosefefrati6547@gmail.com>
2025-02-18 15:17:01 +0200

    frontent added

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
+import React from 'react';
+import Header from './components/Header';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AddPost from './pages/AddPost';
+import EditPost from './pages/EditPost';
 
 const App = () => {
   return (
-    <div>
-   
-    </div>
-  )
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/add" element={<AddPost />} />
+          <Route path="/edit" element={<EditPost />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ frontEnd/src/pages/editPost.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../components/Header' 
+
+const editPost = () => {
+  return (
+    <>
+    <Header/>
+    <h1>editttttttttttttttttttttttttttttttttttttttttttttttttt</h1>
+    </>
+  )
+}
+
+export default editPost
